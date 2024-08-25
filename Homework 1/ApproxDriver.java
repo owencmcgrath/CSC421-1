325ef6a823b8c8069082dbf4a9e1d5f07e7a4c35
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-08-25 12:38:56 -0500

    working seed generation

--- Homework 1/ApproxDriver.java
+++ Homework 1/ApproxDriver.java
@@ -14,14 +14,19 @@ public class ApproxDriver
 
         System.out.print("Enter the file name: ");
         String filename = input.next();
+        System.out.print("The selected file is: " + filename);
 
-        ApproxGenerator approx = new ApproxGenerator(filename);
+        System.out.println("\nEnter the desired order (>= 1): ");
+        int desiredOrder = input.nextInt();
+        System.out.print("The desired order is: " + desiredOrder);
+
+        ApproxGenerator approx = new ApproxGenerator(filename, desiredOrder);
 
         System.out.print("\nEnter the desired string length (negative to quit): ");
         int numChars = input.nextInt();
         while (numChars >= 0)
         {
-            System.out.println(approx.generate(numChars));
+            System.out.println(approx.generate(numChars, desiredOrder));
 
             System.out.print("\nEnter the desired string length (negative to quit): ");
             numChars = input.nextInt();
--- Homework 1/ApproxGenerator.java
+++ Homework 1/ApproxGenerator.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
 import java.io.File;
 import java.util.Random;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
 
 /**
  * Class that reads in and stores the contents of a text file.
- *   @author Dave Reed
- *   @version 8/20/24
+ * @author Dave Reed
+ * @version 8/20/24
  */
 public class ApproxGenerator
 {
     private String cleanText;
     private Random randy;
+    private String seed;
 
     /**
      * Constructs a FileProcessor object that reads in text from a file. The
      * text is processed to remove non-letters and adjacent spaces, and letters
      * are converted to uppercase.
-     *   @param fileName the file containing the text
+     * @param fileName the file containing the text
      */
-    public ApproxGenerator(String fileName) throws Exception
+    public ApproxGenerator(String fileName, int order) throws Exception
     {
         this.cleanText = "";
         this.randy = new Random();
+        this.seed = "";
 
         Scanner infile = new Scanner(new File(fileName));
-        while (infile.hasNextLine()) {
+        while (infile.hasNextLine())
+        {
             String nextLine = infile.nextLine().toUpperCase();
             for (int i = 0; i < nextLine.length(); i++)
             {
@@ -37,18 +43,35 @@ public class ApproxGenerator
             this.cleanText += " ";
         }
         this.cleanText = this.cleanText.trim().replaceAll("\\s+", " ");
+        System.out.println(cleanText);
+
+        //get a random integer and then get the character at that integer
+        int randomInteger = randy.nextInt(cleanText.length() - (order - 1));
+        System.out.println(randomInteger);
+
+        for (int i = randomInteger; i < randomInteger + order; i++)
+        {
+            seed += cleanText.charAt(i);
+        }
+        System.out.println(seed);
+
         infile.close();
     }
 
     /**
      * Generates a string of the specified order and length, matching the
      * statistical distribution of letters and spaces as the specified file.
-     *   @param order the order for the approximation
-     *   @param numChars the length of the generated string
-     *   @return the generated string
+     * @param numChars the length of the generated string
+     * @param order the order for the approximation
+     * @return the generated string
      */
-    public String generate(int numChars)
+    public String generate(int numChars, int order)
     {
+        List<String> listOfFollowinCharacters = new LinkedList<>();
+        HashMap<String, LinkedList<Character>> orderStorage = new HashMap<String, LinkedList<Character>>();
+
+
+
         String newText = "";
         while (newText.length() < numChars)
         {
