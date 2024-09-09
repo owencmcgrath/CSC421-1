84969d067d8692bd001e12c075966c186978f197
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-09-09 17:33:28 -0500

    added i/o

---
+++ Homework 2/KnapsackDriver.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class KnapsackDriver
+{
+    public static void main(String[] args) throws Exception
+        {
+            Scanner input = new Scanner(System.in);
+
+            //getting the weight limit
+            System.out.println("What is the weight limit of the knapsack?: ");
+            Integer weightLimit = input.nextInt();
+
+            //read file in, cleaning is done here
+            System.out.print("Enter the item's file: ");
+            String filename = input.next();
+            input.close();
+
+`
+
+        }
+}
