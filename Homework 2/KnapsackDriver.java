a50bcdf058314d6285d628ac9550ca1a8910bdf1
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-09-09 17:55:14 -0500

    added special item type

--- Homework 2/KnapsackDriver.java
+++ Homework 2/KnapsackDriver.java
@@ -3,19 +3,17 @@ import java.util.*;
 public class KnapsackDriver
 {
     public static void main(String[] args) throws Exception
-        {
-            Scanner input = new Scanner(System.in);
+    {
+        Scanner input = new Scanner(System.in);
 
-            //getting the weight limit
-            System.out.println("What is the weight limit of the knapsack?: ");
-            Integer weightLimit = input.nextInt();
+        //getting the weight limit
+        System.out.println("What is the weight limit of the knapsack?: ");
+        Integer weight = input.nextInt();
 
-            //read file in, cleaning is done here
-            System.out.print("Enter the item's file: ");
-            String filename = input.next();
-            input.close();
+        //read file in, cleaning is done here
+        System.out.print("Enter the item's file: ");
+        String filename = input.next();
+        input.close();
 
-`
-
-        }
+    }
 }
--- Homework 2/KnapsackItem.java
+++ Homework 2/KnapsackItem.java
@@ -2,5 +2,9 @@ import java.util.*;
 
 public class KnapsackItem
 {
-
+    public List readList(String file)
+    {
+        ArrayList<Item> items = new ArrayList<Item>();
+        return items;
+    }
 }
