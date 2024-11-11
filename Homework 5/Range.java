b1b221d4b6582909085bc9f71d72114192d343a9
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-11-11 14:54:18 -0600

    i cannot lose this

--- Homework 5/Range.java
+++ Homework 5/Range.java
@@ -33,40 +33,109 @@ Puzzle
     public boolean 
     hasConflict(int row, int col)
     {
-        String piece = this.grid[row][col];
-
-        if (!piece.matches("\\d+")) //regex to check if the piece is numeric or not. learned at intnerhsip
+        if (grid[row][col].equals("▣")) 
         {
-            return false;
+            if (col > 0 && grid[row][col-1].equals("▣"))
+            {
+                return true;
+            }
+            if (col < numCols() - 1 && grid[row][col+1].equals("▣"))
+            {
+                return true;
+            }
+            if (row > 0 && grid[row-1][col].equals("▣"))
+            {
+                return true;
+            }
+            if (row < numRows() - 1 && grid[row+1][col].equals("▣"))
+            {
+                return true;
+            }
         }
+
+        //a number has been palced, check if it is valid
+        String currentCell = grid[row][col];
+        if (!currentCell.equals("▣") && !currentCell.equals("-")) 
+        {
+            try 
+            {
+                Integer.parseInt(currentCell);
+                if (!checkNum(row, col)) 
+                {
+                    return true;
+                }
+            } 
+            catch (NumberFormatException e) 
+            {
+                //skip if it isn't a number
+            }
+        }
+    
+        for (int i = 0; i < numRows(); i++) 
+        {
+            for (int j = 0; j < numCols(); j++) 
+            {
+                if ((i == row && j == col) || grid[i][j].equals("-") || grid[i][j].equals("▣")) 
+                {
+                    continue;
+                }
+                try 
+                {
+                    Integer.parseInt(grid[i][j]);
+                    if (!checkNum(i, j)) 
+                    {
+                        return true;
+                    }
+                } 
+                catch (NumberFormatException e) 
+                {
+                    continue;
+                }
+            }
+        }
+        return false; 
+    }
+
+    public boolean
+    checkNum(int row, int col)
+    {
+        String piece = this.grid[row][col];
         
         int pieceAsInteger = Integer.parseInt(piece); //convert to integer so that it can be compared to white count
         int whiteCount = 1; //initialize at one to count the cell itself
 
         //starts at the left of the cell and moves left until it hits either a black box or an edge
-        for (int i = col - 1; i >= 0 && grid[row][i].equals("▢"); i--)
+        for (int i = col - 1; i >= 0 && !grid[row][i].equals("▣") && !grid[row][i].equals("-"); i--)
         {
+            System.out.println("Increment Count Left");
             whiteCount++;
         }
 
         //starts at the right of the the cell and moves right until it hits either a black box or an edge
-        for (int i = col + 1; i < numCols && grid[row][i].equals("▢"); i++)
+        for (int i = col + 1; i < numCols && !grid[row][i].equals("▣") && !grid[row][i].equals("-"); i++)
         {
+            System.out.println("Increment Count Right");
             whiteCount++;
         }
 
         //starts at the row abov the cell and moves up until it either hits a black box or an edge
-        for (int i = row - 1; i >= 0 && grid[i][col].equals("▢"); i--)
+        for (int i = row - 1; i >= 0 && !grid[i][col].equals("▣") && !grid[i][col].equals("-"); i--)
         {
+            System.out.println("Increment Count Down");
             whiteCount++;
         }
 
         //starts at the row below the cell and moves down until it either hits a black or an edge
-        for (int i = row + 1; i < numRows && grid[i][col].equals("▢"); i++)
+        for (int i = row + 1; i < numRows && !grid[i][col].equals("▣") && !grid[i][col].equals("-"); i++)
         {
+            System.out.println("Increment Count Up");
             whiteCount++;
         }
-
-        return (whiteCount != pieceAsInteger); //return whether or not the count of white cells doesn't equal the original number  
+        return (whiteCount <= pieceAsInteger); //return whether or not the count of white cells doesn't equal
     }
 }
+
+
+
+
+
