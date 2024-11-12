4f4a188081220a135f6cdfacebac409cab520c39
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-11-11 22:56:58 -0600

    im him

--- Homework 5/Range.java
+++ Homework 5/Range.java
@@ -46,116 +46,120 @@ Puzzle
                 } 
             }  
         }
-
-        if (grid[row][col].equals("▣")) 
-        {
-            if (col > 0 && grid[row][col-1].equals("▣"))
-            {
-                return true;
-            }
-            if (col < numCols() - 1 && grid[row][col+1].equals("▣"))
-            {
-                return true;
-            }
-            if (row > 0 && grid[row-1][col].equals("▣"))
-            {
-                return true;
-            }
-            if (row < numRows() - 1 && grid[row+1][col].equals("▣"))
-            {
-                return true;
-            }
-        }
-        return false;
+        return hasAdjacentBlack(row, col);
     }
 
     /**
-    * Method that checks if      
-    *
-    *
+    * Method that checks if the number is valid based on iteration in every direction
+    *   @param row the row that is to be checked
+    *   @col the column that is to be checked
+    *   @return check to see if the number is valid
     */
-    public boolean
-    numIsValid(int row, int col)
+    private boolean 
+    numIsValid(int row, int col) 
     {
         String piece = this.grid[row][col];
-        int pieceAsInteger = Integer.parseInt(piece); //convert to integer so that it can be compared to white count
+        int pieceAsInteger = Integer.parseInt(piece);
         
-        int whiteCount = 1; //initialize at one to count the cell itself
-        int possible = 0;
-        boolean foundDash = false;
-        
-        //starts at the left of the cell and moves left until it hits either a black box or an edge
-        for (int i = col - 1; i >= 0 && !grid[row][i].equals("▣"); i--)
-        {
-            if (grid[row][i].equals("-"))
-            {
-                foundDash = true;
-            }
-            if (foundDash == false)
-            {
-                whiteCount++;   
-            }
-            else
-            {
-                possible++;
-            }
-        }
+        int whiteCount = 1; //initailize at one to count the cell itself
+        int possible = 0; //possible cells to be checked
 
-        //starts at the right of the the cell and moves right until it hits either a black box or an edge
-        foundDash = false;
-        for (int i = col + 1; i < numCols && !grid[row][i].equals("▣"); i++)
-        {
-            if (grid[row][i].equals("-"))
-            {
-                foundDash = true;
-            }
-            if (foundDash == false)
-            {
-                whiteCount++;   
-            }
-            else
-            {
-                possible++;
-            }
-        }
+        //0 means to not move and > 1 means to move in a certain direction
+        int[] leftCount = countSpaces(row, col, 0, -1);
+        int[] rightCount = countSpaces(row, col, 0, 1);
+        int[] upCount = countSpaces(row, col, -1, 0);
+        int[] downCount = countSpaces(row, col, 1, 0);
 
-        foundDash = false;
-        //starts at the row abov the cell and moves up until it either hits a black box or an edge
-        for (int i = row - 1; i >= 0 && !grid[i][col].equals("▣"); i--)
-        {
-            if (grid[i][col].equals("-"))
-            {
-                foundDash = true;
-            }
-            
-            if (foundDash == false)
-            {
-                whiteCount++;   
-            }
-            else
-            {
-                possible++;
-            }
-        }
+        //number of white spaces found (zeroth place) and all the possible spaces (first place)
+        whiteCount += leftCount[0] + rightCount[0] + upCount[0] + downCount[0];
+        possible += leftCount[1] + rightCount[1] + upCount[1] + downCount[1];
 
-        foundDash = false;
-        //starts at the row below the cell and moves down until it either hits a black or an edge
-        for (int i = row + 1; i < numRows && !grid[i][col].equals("▣"); i++)
-        {
-            if (grid[i][col].equals("-"))
-            {
-                foundDash = true;
-            }
-            
-            if (foundDash == false)
-            {
-                whiteCount++;   
-            }
-            else
-            {
-                possible++;
-            }
-        }
+        //check taht we determined togehter
         return (whiteCount <= pieceAsInteger && (whiteCount + possible) >= pieceAsInteger);
     }
+
+    /**
+    * Helper method that counts the number of spaces
+    *   @param row the row that is to be checked
+    *   @param col the colum to be checked
+    *   @param rowStep iterating the row
+    *   @param colStep iterating the col
+    *   @return a new array with the white and possible count
+    */
+    private int[] 
+    countSpaces(int row, int col, int rowStep, int colStep) 
+    {
+        int white = 0;
+        int possible = 0;
+        boolean foundDash = false;
+
+        //udpates position when checking the next cell, replaces for loops
+        row += rowStep;
+        col += colStep;
+
+        //while we aren't off the edge and it doesn't equal a black square    
+        while (row >= 0 && row < numRows() && col >= 0 && col < numCols() && !grid[row][col].equals("▣")) 
+        {
+            if (grid[row][col].equals("-")) 
+            {
+                foundDash = true;
+            }
+
+            if (!foundDash) 
+            {
+                white++;
+            } 
+            else 
+            {
+                possible++;
+            }
+
+            //continue the iteration
+            row += rowStep;
+            col += colStep;
+        }
+        //creates and returns a new int array with the white and possible values stored
+        return new int[] {white, possible};
+    }
+
+    /**
+    * Checks for an adjacent black sqaure
+    *   @param row the row to be checked
+    *   @param col the col to be chacked
+    *   @return true if there is an adjacent black square,else false
+    */
+    private boolean 
+    hasAdjacentBlack(int row, int col) 
+    {
+    
+        if (!grid[row][col].equals("▣")) 
+        {
+            return false;
+        }
+
+        //move left
+        if (col > 0 && grid[row][col-1].equals("▣")) 
+        {
+            return true;
+        }
+
+        //move right
+        if (col < numCols() - 1 && grid[row][col+1].equals("▣")) 
+        {
+            return true;
+        }
+
+        //move up
+        if (row > 0 && grid[row-1][col].equals("▣")) {
+            return true;
+        }
+
+        //move down
+        if (row < numRows() - 1 && grid[row+1][col].equals("▣")) 
+        {
+            return true;
+        }
+        return false;
+    }
 }
+
