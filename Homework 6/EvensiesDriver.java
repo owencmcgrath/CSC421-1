74e56e7da621015a74eba5ceb7cd9f68d4cc6a9c
owenc-mcgrath <owen.c.mcgrath@icloud.com>
2024-12-02 17:06:54 -0600

    refactor: add detailed comments to methods in EvensiesDriver for improved code clarity

--- Homework 6/EvensiesDriver.java
+++ Homework 6/EvensiesDriver.java
@@ -16,10 +16,16 @@ EvensiesDriver
       int tokens = promptForInput(scan, "Enter the number of tokens: ");
       int rounds = promptForInput(scan, "Enter the number of rounds: ");
       int choice = displayMenuAndGetChoice(scan);
-
       calculate(tokens, rounds, choice);
     }
   }
+
+  /**
+   * Prompts the user for input
+   * @param scan the scanner object
+   * @param prompt the prompt to display
+   * @return the user's input
+   */
   private static
   int promptForInput
   (Scanner scan, String prompt)
@@ -28,6 +34,11 @@ EvensiesDriver
     return scan.nextInt();
   }
 
+  /**
+   * Displays the menu and gets the user's choice
+   * @param scan the scanner object
+   * @return the user's choice
+   */
   private static
   int displayMenuAndGetChoice
   (Scanner scan)
@@ -40,6 +51,12 @@ EvensiesDriver
     return scan.nextInt();
   }
 
+  /**
+   * Calculates the expected tokens based on the user's choice
+   * @param tokens the number of tokens
+   * @param rounds the number of rounds
+   * @param choice the user's choice
+   */
   private static 
   void calculate
   (int tokens, int rounds, int choice)
@@ -50,11 +67,17 @@ EvensiesDriver
       case 0 -> runAllMethods(tokens, rounds, timer);
       case 1 -> runTopDown(tokens, rounds, timer);
       case 2 -> runBottomUp(tokens, rounds, timer);
-      case 3 -> runAllMethods(tokens, rounds, timer);
+      case 3 -> runCaching(tokens, rounds, timer);
       default -> System.out.println("Invalid choice, please try again.");
     }
   }
 
+  /**
+   * Runs all three methods
+   * @param tokens the number of tokens
+   * @param rounds the number of rounds
+   * @param timer the timer object
+   */
   private static
   void runAllMethods
   (int tokens, int rounds, StopWatch timer)
@@ -64,6 +87,12 @@ EvensiesDriver
     runCaching(tokens, rounds, timer);
   }
 
+  /**
+   * Runs the top-down method
+   * @param tokens the number of tokens
+   * @param rounds the number of rounds
+   * @param timer the timer object
+   */
   private static
   void runTopDown
   (int tokens, int rounds, StopWatch timer)
@@ -74,6 +103,12 @@ EvensiesDriver
     System.out.println("Time: " + timer.getElapsedTime() + " milliseconds");
   }
 
+  /**
+   * Runs the bottom-up method
+   * @param tokens the number of tokens
+   * @param rounds the number of rounds
+   * @param timer the timer object
+   */
   private static
   void runBottomUp
   (int tokens, int rounds, StopWatch timer)
@@ -84,6 +119,12 @@ EvensiesDriver
     System.out.println("Time: " + timer.getElapsedTime() + " milliseconds");
   }
   
+  /**
+   * Runs the top-down method with caching
+   * @param tokens the number of tokens
+   * @param rounds the number of rounds
+   * @param timer the timer object
+   */
   private static
   void runCaching
   (int tokens, int rounds, StopWatch timer)
