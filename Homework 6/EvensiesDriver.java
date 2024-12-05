ad2eb097698a694e836f835e9b332b653cee58f4
Owen McGrath <owen.c.mcgrath@icloud.com>
2024-12-05 10:38:19 -0600

    Update EvensiesDriver.java

--- Homework 6/EvensiesDriver.java
+++ Homework 6/EvensiesDriver.java
@@ -1,77 +1,98 @@
 import java.util.Scanner;
 
 /**
- * This class is the driver class for the Evensies game. It prompts the user to enter the number of tokens and rounds
+ * This class is the driver class for the Evensies game.
+ * It prompts the user to enter the number of tokens and rounds adn then calculates based on the selected method.
  * @author Owen McGrath
  * @version 11/19/2024
  */
-public class
-EvensiesDriver
+public class EvensiesDriver
 {
-  public static
-  void main(String[] args)
-  {
-    try (Scanner scan = new Scanner(System.in)) 
+    /**
+    * Main method that takes the input/output for the program
+    * @param args arguments
+    */
+    public static void main(String[] args)
     {
-      System.out.println("Enter the number of tokens: ");
-      int tokens = scan.nextInt();
-
-      System.out.println("Enter the number of rounds: ");
-      int rounds = scan.nextInt();
-
-      System.out.println("Choose the method to calculate expected tokens:");
-      System.out.println("0. All Three");
-      System.out.println("1. Top-Down");
-      System.out.println("2. Bottom-Up");
-      System.out.println("3. Top-Down with Caching");
-      int choice = scan.nextInt();
-
-      StopWatch timer = new StopWatch();
-
-      switch (choice)
-      {
-        case 0 -> 
+        try (Scanner scan = new Scanner(System.in))
         {
-            timer.start();
-            double topDown = Evensies.expectedTopDown(tokens, rounds);
-            timer.stop();
-            System.out.println("Top-down: " + topDown + " (time: " + timer.getElapsedTime() + " ms)");
+            System.out.println("Enter the number of tokens and rounds, respectively: ");
+            int tokens = scan.nextInt();
+            int rounds = scan.nextInt();
 
-            timer.reset();
-            timer.start();
-            double bottomUp = Evensies.expectedBottomUp(tokens, rounds);
-            timer.stop();
-            System.out.println("Bottom-up: " + bottomUp + " (time: " + timer.getElapsedTime() + " ms)");
+            System.out.println("Choose the method to calculate expected tokens:");
+            System.out.println("0. All Three");
+            System.out.println("1. Top-Down");
+            System.out.println("2. Bottom-Up");
+            System.out.println("3. Top-Down with Caching: ");
+            int choice = scan.nextInt();
 
-            timer.reset();
-            timer.start();
-            double caching = Evensies.expectedCaching(tokens, rounds);
-            timer.stop();
-            System.out.println("Caching: " + caching + " (time: " + timer.getElapsedTime() + " ms)");
+            switch (choice)
+            {
+                case 0 -> runAllMethods(tokens, rounds);
+                case 1 -> runTopDown(tokens, rounds);
+                case 2 -> runBottomUp(tokens, rounds);
+                case 3 -> runCaching(tokens, rounds);
+                default -> System.out.println("Invalid choice, please try again.");
+            }
         }
-        case 1 -> 
-        {
-            timer.start();
-            double topDown = Evensies.expectedTopDown(tokens, rounds);
-            timer.stop();
-            System.out.println("Top-down: " + topDown + " (time: " + timer.getElapsedTime() + " ms)");
-        }
-        case 2 -> 
-        {
-            timer.start();
-            double bottomUp = Evensies.expectedBottomUp(tokens, rounds);
-            timer.stop();
-            System.out.println("Bottom-up: " + bottomUp + " (time: " + timer.getElapsedTime() + " ms)");
-        }
-        case 3 -> 
-        {
-            timer.start();
-            double caching = Evensies.expectedCaching(tokens, rounds);
-            timer.stop();
-            System.out.println("Caching: " + caching + " (time: " + timer.getElapsedTime() + " ms)");
-        }
-        default -> System.out.println("Invalid choice. Please restart the program and choose a valid option.");
-      } 
     }
-  }
+
+    /**
+     * Runs all three methods
+     * @param tokens the number of tokens
+     * @param rounds the number of rounds
+     * @param timer the timer object
+     */
+    private static void runAllMethods(int tokens, int rounds)
+    {
+        runTopDown(tokens, rounds);
+        runBottomUp(tokens, rounds);
+        runCaching(tokens, rounds);
+    }
+
+    /**
+     * Runs the top-down method
+     * @param tokens the number of tokens
+     * @param rounds the number of rounds
+     * @param timer the timer object
+     */
+    private static void runTopDown(int tokens, int rounds)
+    {
+        StopWatch timer = new StopWatch();
+        timer.start();
+        System.out.println("Top-Down: " + Evensies.expectedTopDown(tokens, rounds));
+        timer.stop();
+        System.out.println("Time: " + timer.getElapsedTime() + " milliseconds");
+    }
+
+    /**
+     * Runs the bottom-up method
+     * @param tokens the number of tokens
+     * @param rounds the number of rounds
+     * @param timer the timer object
+     */
+    private static void runBottomUp(int tokens, int rounds)
+    {
+        StopWatch timer = new StopWatch();
+        timer.start();
+        System.out.println("Bottom-Up: " + Evensies.expectedBottomUp(tokens, rounds));
+        timer.stop();
+        System.out.println("Time: " + timer.getElapsedTime() + " milliseconds");
+    }
+
+    /**
+     * Runs the top-down method with caching
+     * @param tokens the number of tokens
+     * @param rounds the number of rounds
+     * @param timer the timer object
+     */
+    private static void runCaching(int tokens, int rounds)
+    {
+        StopWatch timer = new StopWatch();
+        timer.start();
+        System.out.println("Top-Down with Caching: " + Evensies.expectedCaching(tokens, rounds));
+        timer.stop();
+        System.out.println("Time: " + timer.getElapsedTime() + " milliseconds");
+    }
 }
